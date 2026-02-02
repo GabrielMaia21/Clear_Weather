57abb89b98a4283256fdb8080703d6850f50a51d
Felipe Gomes <felipegstech@gmail.com>
2026-02-02 17:54:04 -0300

    feat: autocomplete na barra de busca

---
+++ src/Services/GeoApi.js
@@ -0,0 +1,42 @@
+// API para o AutoComplete da barra de pesquisa
+
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY2;
+
+export async function buscarCidade(cidade) {
+  try {
+    const resposta = await axios.get(
+      "http://api.openweathermap.org/geo/1.0/direct",
+      {
+        params: {
+          q: cidade,
+          limit: 5,
+          appid: API_KEY,
+        },
+      },
+    );
+
+    const data = resposta.data;
+    let infos = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].name && data[i].country) {
+        let jaExiste = false;
+
+        for(let j = 0; j < infos.length; j++) {
+          if(infos[j].cidade == data[i].name && infos[j].pais == data[i].country) {jaExiste = true};
+        }
+
+        if (!jaExiste) {
+          infos.push({ cidade: data[i].name, pais: data[i].country });
+        }
+      }
+    }
+
+    return infos;
+  } catch (error) {
+    console.error("Erro na requisição da API Geocode. Erro: ", error);
+    return [];
+  }
+}
